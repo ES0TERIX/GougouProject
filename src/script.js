31d38d3311a2f8189bbface04dbccd99ea1d8ca9
ES0TERIX <thomas.carre74@gmail.com>
2023-10-14 22:52:16 +0200

    add Burger Menu + modify to Header Icon (also make change to ul) *tried to implement animation but it sucks

--- src/script.js
+++ src/script.js
@@ -14,3 +14,21 @@ gsap.utils.toArray(".panel").forEach((section) => {
     },
   });
 });
+
+var tl = gsap.timeline();
+
+const nexus = document.querySelector(".logo-nexus");
+
+tl.to(".nexus-link", {
+  translateY: "100%",
+  duration: 0.5,
+});
+
+tl.to(".nexus", {
+  width: "0",
+});
+
+nexus.addEventListener("click", () => {
+  console.log("test");
+  tl.reversed(!tl.reversed());
+});
