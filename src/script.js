8bfdc75ccb253e59c494a3e0be4db8a1b80a719d
gaetan <Nuxnuxx>
2023-10-16 09:37:05 +0200

    fix menu

--- src/script.js
+++ src/script.js
@@ -4,31 +4,32 @@ import ScrollTrigger from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 
 gsap.utils.toArray(".panel").forEach((section) => {
-  gsap.to(section, {
-    scrollTrigger: {
-      markers: true,
-      trigger: section,
-      start: "-5% top",
-      end: "bottom 15%",
-      snap: 3,
-    },
-  });
+	gsap.to(section, {
+		scrollTrigger: {
+			markers: true,
+			trigger: section,
+			start: "-5% top",
+			end: "bottom 5%",
+			snap: 3,
+		},
+	});
 });
 
 var tl = gsap.timeline();
 
 const nexus = document.querySelector(".logo-nexus");
-
-tl.to(".nexus-link", {
-  translateY: "100%",
-  duration: 0.5,
-});
-
-tl.to(".nexus", {
-  width: "0",
-});
+// tl.to(".nexus-link", {
+//   translateY: "100%",
+//   duration: 0.5,
+// });
+//
+// tl.to(".nexus", {
+//   width: "0",
+// });
 
 nexus.addEventListener("click", () => {
-  console.log("test");
-  tl.reversed(!tl.reversed());
+	console.log("test");
+	tl.to(".nexus", {
+		left: "0%",
+	});
 });
