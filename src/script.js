6272095f2b958b014794c624a447900018fd9e56
ES0TERIX <thomas.carre74@gmail.com>
2023-10-17 12:23:00 +0200

    working menu animation and scroll snap, add content to section 3 and 4, refacto header

--- src/script.js
+++ src/script.js
@@ -4,32 +4,37 @@ import ScrollTrigger from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 
 gsap.utils.toArray(".panel").forEach((section) => {
-	gsap.to(section, {
-		scrollTrigger: {
-			markers: true,
-			trigger: section,
-			start: "-5% top",
-			end: "bottom 5%",
-			snap: 3,
-		},
-	});
+  gsap.to(section, {
+    scrollTrigger: {
+      scroller: "body",
+      trigger: section,
+      markers: "true",
+      start: "top top",
+      end: "bottom top",
+      snap: {
+        snapTo: 4,
+        duration: 0.5,
+        delay: 0.01,
+      },
+    },
+  });
 });
 
 var tl = gsap.timeline();
-
 const nexus = document.querySelector(".logo-nexus");
-// tl.to(".nexus-link", {
-//   translateY: "100%",
-//   duration: 0.5,
-// });
-//
-// tl.to(".nexus", {
-//   width: "0",
-// });
+var test = 0;
 
 nexus.addEventListener("click", () => {
-	console.log("test");
-	tl.to(".nexus", {
-		left: "0%",
-	});
+  console.log(test);
+  if (test == 0) {
+    tl.to(".nexus", {
+      left: "0%",
+    });
+    test = 1;
+  } else {
+    tl.to(".nexus", {
+      left: "-100%",
+    });
+    test = 0;
+  }
 });
